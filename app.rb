dc19553810dde138f9c82bd72784fc54d0b6523b
Aileen Cacayorin <acacayorin@gmail.com>
2015-08-25 21:50:26 -0700

    add update form to product page

--- app.rb
+++ app.rb
@@ -19,8 +19,25 @@ end
 post('/products') do
   @product = Product.new({:name => params.fetch('product_name'), :price => params.fetch('product_price').to_f, :purchase_id => nil, :purchased =>false})
   if @product.save
-    erb(:success)
+    redirect('/products/'.concat(@product.id).to_s)
   else
     erb(:errors)
   end
+end
+
+get('/products/:id') do
+  id = params.fetch('id').to_i
+  @product = Product.find(id)
+  erb(:product_info)
+end
+
+patch('/products/:id') do
+  id = params.fetch('id').to_i
+  @product = Product.find(id)
+
+  new_name = params.fetch('name')
+  new_price = params.fetch('price').to_f
+  @product.update_attributes(:name => new_name, :price => new_price)
+
+  erb(:success)
 end
